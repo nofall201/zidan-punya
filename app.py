b85737890312d4ca7859738882c327669b3c843f
Zhiddan Aditya <135048801+Zhizuuu@users.noreply.github.com>
2025-06-25 02:13:29 +0700

    Update .gitignore and cleanup tracked files

--- app.py
+++ app.py
@@ -1138,7 +1138,7 @@ def get_accuracy_by_label():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Membuat tabel jika belum ada
-    app.run(debug=True, host='0.0.0.0', port=int(environ.get('PORT', 5000))))
+    app.run(debug=True, host='0.0.0.0', port=int(environ.get('PORT', 5000)))
 
 
 
